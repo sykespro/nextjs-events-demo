ffda6287c29bde20159751af2a449b4817db01f8
Daniel Sykes <sykespro@gmail.com>
2021-07-08 18:10:52 -0400

    added newsletter api and components

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,62 +1,65 @@
 import { Fragment } from "react";
-import Head from 'next/head';
-import EventSummary from '../../components/event-detail/event-summary';
-import EventLogisticis from '../../components/event-detail/event-logistics';
-import EventContent from '../../components/event-detail/event-content';
+import Head from "next/head";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogisticis from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
+import Comments from "../../components/input/comments";
 
 const DetailedEventPage = (props) => {
-    const event = props.selectedEvent;
+	const event = props.selectedEvent;
 
-    if (!event) {
-        return (
-            <div className='center'>
-                <p>Loading...</p>
-            </div>
-        )
-    }
+	if (!event) {
+		return (
+			<div className="center">
+				<p>Loading...</p>
+			</div>
+		);
+	}
 
-    return (
-        <Fragment>
-            <Head>
-                <title>{event.title}</title>
-                <meta name="description" content={event.description} />
-            </Head>
-            <EventSummary title={event.title} />
-            <EventLogisticis
-                date={event.date}
-                address={event.location}
-                image={event.image}
-                imageAlt={event.title} />
-            <EventContent>
-                <p>{event.description}</p>
-            </EventContent>
-        </Fragment>
-    )
+	return (
+		<Fragment>
+			<Head>
+				<title>{event.title}</title>
+				<meta name="description" content={event.description} />
+			</Head>
+			<EventSummary title={event.title} />
+			<EventLogisticis
+				date={event.date}
+				address={event.location}
+				image={event.image}
+				imageAlt={event.title}
+			/>
+			<EventContent>
+				<p>{event.description}</p>
+			</EventContent>
+			<Comments eventId={event.id} />
+		</Fragment>
+	);
 };
 
 export async function getStaticProps(context) {
-    const eventId = context.params.id;
+	const eventId = context.params.id;
 
-    const event = await getEventById(eventId);
+	const event = await getEventById(eventId);
 
-    return {
-        props: {
-            selectedEvent: event
-        },
-        revalidate: 30
-    };
+	return {
+		props: {
+			selectedEvent: event,
+		},
+		revalidate: 30,
+	};
 }
 
 export async function getStaticPaths() {
-    const events = await getFeaturedEvents();
+	const events = await getFeaturedEvents();
 
-    const paths = events.map(event => ({ params: { id: event.id } }));
+	const paths = events.map((event) => ({ params: { id: event.id } }));
 
-    return {
-        paths: paths,
-        fallback: 'blocking'
-    };
+	return {
+		paths: paths,
+		fallback: "blocking",
+	};
 }
 
 export default DetailedEventPage;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
-import Head from 'next/head';
-import EventList from '../components/events/event-list';
-import { getFeaturedEvents } from '../helpers/api-util';
+import Head from "next/head";
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../helpers/api-util";
+import NewsletterRegistration from "../components/input/newsletter-registration";
 
 export default function Home(props) {
-
-  return (
-    <div>
-      <Head>
-        <title>NextJS Events</title>
-        <meta name="description" content="Cool events" />
-      </Head>
-      <EventList items={props.events} />
-    </div>
-  )
+	return (
+		<div>
+			<Head>
+				<title>NextJS Events</title>
+				<meta name="description" content="Cool events" />
+			</Head>
+			<NewsletterRegistration />
+			<EventList items={props.events} />
+		</div>
+	);
 }
 
 export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents();
+	const featuredEvents = await getFeaturedEvents();
 
-  return {
-    props: {
-      events: featuredEvents
-    },
-    revalidate: 1800
-  }
+	return {
+		props: {
+			events: featuredEvents,
+		},
+		revalidate: 1800,
+	};
 }
