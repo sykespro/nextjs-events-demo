e40a4e2a7eba482306999446c79af80ef96da3dd
Daniel Sykes <sykespro@gmail.com>
2021-07-08 19:47:05 -0400

    added comments

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,31 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
-import CommentList from './comment-list';
-import NewComment from './new-comment';
-import classes from './comments.module.css';
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import classes from "./comments.module.css";
 
 function Comments(props) {
-  const { eventId } = props;
+	const { eventId } = props;
 
-  const [showComments, setShowComments] = useState(false);
+	const [showComments, setShowComments] = useState(false);
+	const [comments, setComments] = useState([]);
 
-  function toggleCommentsHandler() {
-    setShowComments((prevStatus) => !prevStatus);
-  }
+	useEffect(() => {
+		fetch(`/api/comments/${eventId}`)
+			.then((response) => response.json())
+			.then((data) => {
+				setComments(data.comments);
+			});
+	}, [showComments]);
 
-  function addCommentHandler(commentData) {
-    // send data to API
-  }
+	function toggleCommentsHandler() {
+		setShowComments((prevStatus) => !prevStatus);
+	}
 
-  return (
-    <section className={classes.comments}>
-      <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
-      </button>
-      {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
-    </section>
-  );
+	function addCommentHandler(commentData) {
+		// send data to API
+		fetch(`/api/comments/${eventId}`, {
+			method: "POST",
+			body: JSON.stringify(commentData),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	}
+
+	return (
+		<section className={classes.comments}>
+			<button onClick={toggleCommentsHandler}>
+				{showComments ? "Hide" : "Show"} Comments
+			</button>
+			{showComments && <NewComment onAddComment={addCommentHandler} />}
+			{showComments && <CommentList items={comments} />}
+		</section>
+	);
 }
 
 export default Comments;
